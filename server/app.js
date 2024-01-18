2ce878a5f4389c34e6824639d2268358459bf16d
MohammadSharifAnsari <mohammadsharifansari157@gmail.com>
2024-01-19 00:36:38 +0530

    added routes

--- server/app.js
+++ server/app.js
@@ -4,18 +4,36 @@ import Express, { json } from "express";
 import dotenv from "dotenv/config"
 import errorMiddleware from "./Middleware/error.middleware.js";
 import cors from "cors"
-
+import userRoutes from './Routes/user.routes.js'
+import productRoutes from "./Routes/product.routes.js";
+import paymentRoutes from "./Routes/payment.routes.js";
+import cartRoutes from "./Routes/cart.routes.js";
+import orderRoutes from "./Routes/order.routes.js"
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 const app=Express();
 
-
+app.use(Express.json());
+app.use(Express.urlencoded({extended:true}));
+app.use(morgan('dev'));
+app.use(cookieParser());
 
 app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }))
   
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+  credentials:true
+}))
+
+
+app.use('/api/v1/user',userRoutes);
+app.use('/api/v1/product',productRoutes);
+app.use('/api/v1/order',orderRoutes);
+app.use('/api/v1/cart',cartRoutes);
+app.use('/api/v1/payment',paymentRoutes);
+
 
 app.all('*',(req,res,next)=>{
 
